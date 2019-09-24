6c5fe20035651b53208de51c8a1714868cc704c7
claire <wonderfulsuccess@163.com>
2019-09-25 01:04:07 +0800

    123

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print('This script is used to tell github: I am a python repo')
+    print('This script is used to tell github: I am a python repo. Contact author to get source code')
 
 if __name__ == '__main__':
     main()
